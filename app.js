746cd253aacc87b023c277e7854cca76fff56746
Mike Norris <minorris513@gmail.com>
2024-01-18 09:38:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const { PORT = 3001 } = process.env;
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
+  console.log("connected to db", r);
+});
+app.listen(PORT, () => {
+  console.log("App listening at the port ${PORT}");
+  console.log("this is working");
+});
