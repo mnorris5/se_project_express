a9502d1e03cbb47c7df9d00f31e4ca98935ec54f
Mike Norris <minorris513@gmail.com>
2024-09-10 14:53:57 -0400

    added auth

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const auth = require("./middleware/auth");
 
 const app = express();
 const { PORT = 3001 } = process.env;
